44f30ded92d91ebf56e81933972844961bc421ca
Christian Durazo <ctdurazo@gmail.com>
2019-08-06 13:36:35 -0700

    init adding socket io

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask_socketio import SocketIO, send
+
 from Games import blackjack
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secretkey'
+socketio = SocketIO(app)
 
 
 @app.route('/')
@@ -16,12 +20,30 @@ def twentyone():
 	deck = blackjack.newdeck(numdecks)
 	hands = blackjack.deal(numplayers, deck)
 	out = ''
-	for i in range(0, numplayers):
+	for i in range(0, numplayers-1):
 		out = out + '<p>' + hands[i]['cards'][0].description() + ', ' \
-		+ hands[i]['cards'][1].description() + ' = ' \
-		+ str(blackjack.valueofhand(hands[i]['cards'])) + '</p>'
+			+ hands[i]['cards'][1].description() + ' = ' \
+			+ str(blackjack.valueofhand(hands[i]['cards'])) + '</p>'
+	out = out + '<p>' + hands[numplayers-1]['cards'][0].description() + ', ' \
+		+ str(hands[numplayers-1]['cards'][0].value(0)) + '</p>'
 	return out
 
 
+def messagereceived(methods=['GET', 'POST']):
+	print('message was received!!!')
+
+
+@socketio.on('messageSent')
+def handle_messagesent(json, methods=['GET', 'POST']):
+	print('Message Sent: ' + str(json))
+	socketio.emit('message', json, callback=messagereceived)
+
+
+@socketio.on('message')
+def handle_message(msg):
+	print('Message: ' + msg)
+	send(msg, broadcast=True)
+
+
 if __name__ == '__main__':
-	app.run()
+	socketio.run(app)
